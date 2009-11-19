07f180e837fd019fc487da6e166320e27c776717
Hong, MinHee <minhee@dahlia.kr>
2009-11-19 11:51:12 +0900

    Implemented Lisphp_Environment.

---
+++ Lisphp/Environment.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'Lisphp/Scope.php';
+require_once 'Lisphp/Runtime.php';
+
+final class Lisphp_Environment {
+    static function sandbox() {
+        $scope = new Lisphp_Scope;
+        $scope['eval'] = new Lisphp_Runtime_Eval;
+        $scope['quote'] = new Lisphp_Runtime_Quote;
+        $scope['define'] = new Lisphp_Runtime_Define;
+        $scope['lambda'] = new Lisphp_Runtime_Lambda;
+        $scope['apply'] = new Lisphp_Runtime_Apply;
+        $scope['car'] = new Lisphp_Runtime_List_Car;
+        $scope['cdr'] = new Lisphp_Runtime_List_Cdr;
+        $scope['+'] = new Lisphp_Runtime_Arithmetic_Addition;
+        $scope['-'] = new Lisphp_Runtime_Arithmetic_Subtraction;
+        $scope['*'] = new Lisphp_Runtime_Arithmetic_Multiplication;
+        $scope['/'] = new Lisphp_Runtime_Arithmetic_Division;
+        $scope['%'] = new Lisphp_Runtime_Arithmetic_Modulus;
+        $scope['not'] = new Lisphp_Runtime_Logical_Not;
+        $scope['and'] = new Lisphp_Runtime_Logical_And;
+        $scope['or'] = new Lisphp_Runtime_Logical_Or;
+        $scope['if'] = new Lisphp_Runtime_Logical_If;
+        return $scope;
+    }
+}
+
---
+++ Lisphp/Test/EnvironmentTest.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'PHPUnit/Framework.php';
+require_once 'Lisphp/Environment.php';
+require_once 'Lisphp/Runtime.php';
+
+class Lisphp_Test_EnvironmentTest extends PHPUnit_Framework_TestCase {
+    function testSandbox() {
+        $scope = Lisphp_Environment::sandbox();
+        $this->assertType('Lisphp_Runtime_Eval', $scope['eval']);
+        $this->assertType('Lisphp_Runtime_Quote', $scope['quote']);
+        $this->assertType('Lisphp_Runtime_Define', $scope['define']);
+        $this->assertType('Lisphp_Runtime_Lambda', $scope['lambda']);
+        $this->assertType('Lisphp_Runtime_Apply', $scope['apply']);
+        $this->assertType('Lisphp_Runtime_List_Car', $scope['car']);
+        $this->assertType('Lisphp_Runtime_List_Cdr', $scope['cdr']);
+        $this->assertType('Lisphp_Runtime_Arithmetic_Addition', $scope['+']);
+        $this->assertType('Lisphp_Runtime_Arithmetic_Subtraction', $scope['-']);
+        $this->assertType('Lisphp_Runtime_Arithmetic_Multiplication',
+                          $scope['*']);
+        $this->assertType('Lisphp_Runtime_Arithmetic_Division', $scope['/']);
+        $this->assertType('Lisphp_Runtime_Arithmetic_Modulus', $scope['%']);
+        $this->assertType('Lisphp_Runtime_Logical_Not', $scope['not']);
+        $this->assertType('Lisphp_Runtime_Logical_And', $scope['and']);
+        $this->assertType('Lisphp_Runtime_Logical_Or', $scope['or']);
+        $this->assertType('Lisphp_Runtime_Logical_If', $scope['if']);
+    }
+}
+
