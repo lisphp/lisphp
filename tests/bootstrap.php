3059da78bd2be07770a400bea7daa66e299da84c
Igor Wiedler <igor@wiedler.ch>
2012-07-11 22:32:47 +0200

    Remove wrong comment

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,4 @@
 <?php
 
-/*
- * This file is part of Evenement.
- *
- * (c) Igor Wiedler <igor@wiedler.ch>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 $loader = require __DIR__.'/../vendor/autoload.php';
 $loader->add('Lisphp_Test', __DIR__);
