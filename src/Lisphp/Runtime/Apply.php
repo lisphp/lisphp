dada3dad1ddfedae103ca61d0e99755add8b906d
Jonathan Aquino <jon@ning.com>
2014-02-28 19:55:18 -0800

    Made apply work for any Function, not just BuiltinFunctions.
    There's no reason why apply can't work on any Function.

--- src/Lisphp/Runtime/Apply.php
+++ src/Lisphp/Runtime/Apply.php
@@ -5,7 +5,7 @@ final class Lisphp_Runtime_Apply extends Lisphp_Runtime_BuiltinFunction
     protected function execute(array $arguments)
     {
         list($func, $args) = $arguments;
-        if ($func instanceof Lisphp_Runtime_BuiltinFunction) {
+        if ($func instanceof Lisphp_Runtime_Function) {
             return $func->execute($args->getArrayCopy());
         }
     }
